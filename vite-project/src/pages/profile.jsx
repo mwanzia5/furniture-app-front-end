0646cef0a39692ec5d0c1bfb1077d664acc7b764
NoniMuthoni <noni.muthoni.noni@gmail.com>
2024-02-22 21:38:05 +0300

    CORS🙄

--- vite-project/src/pages/profile.jsx
+++ vite-project/src/pages/profile.jsx
@@ -1,49 +1,61 @@
+import { Grid, Box, Badge } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
-import { Box, Heading, Text, Image } from "@chakra-ui/react";
 import { api } from "../assets/utils";
 
-function Profile() {
-  const [userData, setUserData] = useState(null);
+const Profile = () => {
+  const [profiles, setProfiles] = useState([]);
 
+  const fetchProfile = async () => {
+    try {
+      const response = await api.get("/users");
+      setProfiles(response.data);
+    } catch (error) {
+      console.error("Error fecthing profile", error);
+    }
+  };
   useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        // Make a request to backend API endpoint to fetch user data
-        const response = await api.get('/users'); 
-        console.log(response)
-        setUserData(response);
-
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-      }
-    };
-         // Cleanup function
-    fetchUserData();
-    return () => {
-    };
+    fetchProfile();
   }, []);
-
   return (
-    <Box className="profile-container">
-      {/* Render user data here */}
-      {setUserData.map(users => (
-         <Box key= {users.id}>
-         {/* Change to icon */}
-         <Box mb={4}>
-           <Image
-             src={userData.profileImage}
-             alt={userData.username}
-             borderRadius="full"
-             boxSize="150px"
-             objectFit="cover"
-           />
-         </Box>
-         <Heading as="h2">{users.username}</Heading>
-         <Text>Email: {users.email}</Text>
-       </Box>
+    <div>
+      <h1>Profile🗿 </h1>
+      {profiles.map((profile) => (
+        <Grid key={profile.id}>
+          <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+            <Box p="6">
+              <Box display="flex" alignItems="baseline">
+                <Badge borderRadius="full" px="2" colorScheme="teal">
+                  {profile.userName}
+                </Badge>
+                <Box
+                  color="gray.500"
+                  fontWeight="semibold"
+                  letterSpacing="wide"
+                  fontSize="xs"
+                  textTransform="uppercase"
+                  ml="2"
+                >
+                  {profile.email} beds &bull; {profile.role} baths
+                </Box>
+              </Box>
+
+              <Box
+                mt="1"
+                fontWeight="semibold"
+                as="h4"
+                lineHeight="tight"
+                noOfLines={1}
+              >
+                {profile.phone_number}
+              </Box>
+
+              <Box>{profile.created_at}</Box>
+            </Box>
+          </Box>
+        </Grid>
       ))}
-    </Box>
+    </div>
   );
-}
+};
 
 export default Profile;
