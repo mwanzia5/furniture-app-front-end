0a050cc868bf6726c977cd55a507c60040717d03
reagan <reaganm746@gmail.com>
2024-02-22 21:46:35 +0300

    creating header,footer,home

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer';
+import AboutUs from './pages/AboutUs';
+import FAQ from './pages/FAQ';
+import RefundPolicy from './pages/RefundPolicy';
+import ShippingPolicy from './pages/ShippingPolicy';
+import Header from './components/Header';
+import Home from './pages/Home';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+const App = () => {
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/Home" element={<Home />} />
+            
+          </Routes>
+        </Router>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    <Router>
+      <div>
+        <Footer />
+        <Routes>
+          <Route exact path="/AboutUs" element={<AboutUs />} />
+          <Route exact path="/FAQ" element={<FAQ />} />
+          <Route exact path="/RefundPolicy" element={<RefundPolicy />} />
+          <Route exact path="/ShippingPolicy" element={<ShippingPolicy />} />
+        </Routes>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
